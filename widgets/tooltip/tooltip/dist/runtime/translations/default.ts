34e181e402c161727229ead1ded8ff489754c503
MiltonSantos <38756446+MiltonSantos@users.noreply.github.com>
2026-09-15 11:17:57 +0100

    Add files via upload

---
+++ widgets/tooltip/tooltip/dist/runtime/translations/default.ts
@@ -0,0 +1,8 @@
+export default {
+  _widgetLabel: 'Tooltip',
+  defaultTriggerText: 'Help',
+  defaultTooltipTitle: 'Information',
+  defaultTooltipContent: 'Add your helpful description, instructions, or disclaimers here.',
+  learnMore: 'Learn more',
+  placeholderConfigure: 'Please configure tooltip trigger and content in settings to begin.'
+}
